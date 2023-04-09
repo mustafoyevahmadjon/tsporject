de130f143b4aa8f644a22bd0fa2655b7e1027b31
Mustafoyev Ahmadjon <79581200+mustafoyevahmadjon@users.noreply.github.com>
2023-04-09 22:15:52 +0500

    MUI Drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import useState from "react"
+import { useState } from "react"
 import { useQuery } from "react-query";
 
 // Components
 import { LinearProgress } from "@mui/material";
 import { Item } from "./components/Item";
 import Grid from "@mui/material/Grid"
+import Drawer from "@mui/material/Drawer";
+import Badge from '@mui/material/Badge';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 // Styles
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
 
 // Types
 export type cartItemType = {
@@ -23,8 +26,10 @@ const getProducts = async (): Promise<cartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products/")).json()
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([] as cartItemType[])
   const { data, isLoading, error } = useQuery<cartItemType[]>("products", getProducts)
-  const getTotalItems = () => null
+  const getTotalItems = (items: cartItemType[]) => null
   const handleAddToCart = (clickedItem: cartItemType) => null
   const handleRemoveFromCart = () => null
 
@@ -32,10 +37,18 @@ function App() {
   if (error) return <div>Something went wrong ...</div>
   return (
     <Wrapper className="App">
+      <Drawer anchor="right" open={isCartOpen} onClose={() => setIsCartOpen(false)}>
+        Cart Goes Here
+      </Drawer>
+      <StyledButton onClick={() => setIsCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="primary">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
-            <Item item={item} handleAddToCart={handleAddToCart}/>
+            <Item item={item} handleAddToCart={handleAddToCart} />
           </Grid>
         ))}
       </Grid>
